b188f311419315c876e069c31216e90abe2738a5
Dzkzzz <dzdzaakk@gmail.com>
2026-01-14 22:56:37 +0700

    spiral matrix II

---
+++ 01-Array or Matrix Manipulation Pattern/Array or Matrix - Spiral Traversal/spiral-matrix-II.go
@@ -0,0 +1,48 @@
+package main
+
+func generateMatrix(n int) [][]int {
+	matrix := make([][]int, n)
+
+	for i := 0; i < n; i++ {
+		matrix[i] = make([]int, n)
+	}
+
+	top, bottom := 0, n-1
+	left, right := 0, n-1
+
+	num := 1
+	max := n * n
+
+	for num <= max {
+
+		// left to right (top row)
+		for col := left; col <= right; col++ {
+			matrix[top][col] = num
+			num++
+		}
+		top++
+
+		// top to bottom (right column)
+		for row := top; row <= bottom; row++ {
+			matrix[row][right] = num
+			num++
+		}
+		right--
+
+		// right to left (bottom row)
+		for col := right; col >= left; col-- {
+			matrix[bottom][col] = num
+			num++
+		}
+		bottom--
+
+		// bottom to top (left column)
+		for row := bottom; row >= top && num <= max; row-- {
+			matrix[row][left] = num
+			num++
+		}
+		left++
+	}
+
+	return matrix
+}

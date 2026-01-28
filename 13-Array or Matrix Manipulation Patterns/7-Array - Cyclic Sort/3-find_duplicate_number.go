1d7018b0cbfe87e1a3613d12a3ede569cee033ac
Dzkzzz <dzdzaakk@gmail.com>
2026-01-28 23:53:52 +0700

    find duplicate nunmber

---
+++ 13-Array or Matrix Manipulation Patterns/7-Array - Cyclic Sort/3-find_duplicate_number.go
@@ -0,0 +1,24 @@
+package main
+
+func findDuplicate(nums []int) int {
+
+	for i := range nums {
+
+		idx := abs(nums[i]) - 1
+
+		if nums[idx] > 0 {
+			nums[idx] *= -1
+		} else {
+			return abs(nums[i])
+		}
+	}
+	return 0
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
